46efd5b07f4771f9e0f6311a57f993b245aeeca6
MUHAMMAD ZOHAIB <muhammadzohaib21122@gmail.com>
2024-10-16 16:11:15 +0500

    Update app.js

--- app.js
+++ app.js
@@ -24,8 +24,7 @@ const showWinner = (userWin, userChoice, compChoice) => {
     userScorePara.innerText = userScore;
     msg.innerText = `You win! Your ${userChoice} beats ${compChoice}`;
     msg.style.backgroundColor = "green";
-  }
-   else {
+  }else {
     compScore++;
     compScorePara.innerText = compScore;
     msg.innerText = `You lost. ${compChoice} beats your ${userChoice}`;
