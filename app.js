0915c84be81fadd8897b362cd4287f244954de5f
MUHAMMAD ZOHAIB <muhammadzohaib21122@gmail.com>
2024-10-19 09:34:11 +0500

    Update app.js

--- app.js
+++ app.js
@@ -55,9 +55,9 @@ const playGame = (userChoice) => {
   }
  };
 
-// choices.forEach((choice) => {
-//   choice.addEventListener("click", () => {
-//     const userChoice = choice.getAttribute("id");
-//     playGame(userChoice);
-//   });
-// });
+choices.forEach((choice) => {
+  choice.addEventListener("click", () => {
+    const userChoice = choice.getAttribute("id");
+    playGame(userChoice);
+  });
+});
